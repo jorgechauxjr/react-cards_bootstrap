c2b3a95bffc9b9568b5f086717cd1d492a849755
Jorge <chauxjorge@hotmail.com>
2021-08-06 18:25:01 -0500

    Personalize Card and general background page

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,14 @@ import image1 from '../assets/image1.jpg'
 
 function Card() {
   return (
-    <div className="card">
+    <div className="card text-center bg-dark">
       <img src={image1} alt="photo" />
-      <div className="card-body">
+      <div className="card-body text-light">
         <h4 className="card-title">My Title</h4>
         <p className="card-text text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quos. Iste saepe voluptas soluta ipsa voluptatum corporis. Laudantium veritatis impedit ipsum dolor vero sed? Facilis consectetur eligendi doloremque earum neque.</p>
+        <a href='#!' className="btn btn-outline-secondary rounded-0">
+          Go to this web site
+        </a>
       </div>
     </div>
   )
